73e3b5be7c4c2ab5ab8e55cccbe26d7534d6efb5
moonMaster922 <mayankthapliyal100@gmail.com>
2026-02-14 11:20:03 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -4,9 +4,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
-console.log(import.meta.env.VITE_API_URL)
 export const sendMessageAPI = (message, sessionId) =>
-
   API.post("/chat", { message, sessionId });
 export const getHistoryAPI = (sessionId) => API.get(`history/${sessionId}`);
 export const getSessionsAPI = () => API.get(`/sessions`);
